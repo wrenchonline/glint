976e67c6d898cec4861d16d2ed79fdbf60feb7d1
wrenchoniline <ljl260435988@gmail.com>
2021-11-17 16:58:16 +0800

    add Referer  CSRFs  VULNERABLE

--- csrf/csrf.go
+++ csrf/csrf.go
@@ -37,3 +37,34 @@ func Origin(k string, v []interface{}) error {
 	}
 	return nil
 }
+
+func Referer(k string, v []interface{}) error {
+	REFERER_URL := `http://not-a-valid-origin.xsrfprobe-csrftesting.0xinfection.xyz`
+	for _, url := range v {
+		fastReq := fasthttp.AcquireRequest()
+		err := config.CopyConfReq(url, fastReq)
+		if err != nil {
+			return err
+		}
+		resp := &fasthttp.Response{}
+		client := &fasthttp.Client{}
+		if err := client.Do(fastReq, resp); err != nil {
+			fmt.Println("request fail:", err.Error())
+			return err
+		}
+		b1 := resp.Body()
+		fastReq2 := fasthttp.AcquireRequest()
+		fastReq.CopyTo(fastReq2)
+		fastReq2.Header.Set("Referer", REFERER_URL)
+		client2 := &fasthttp.Client{}
+		if err := client2.Do(fastReq, resp); err != nil {
+			fmt.Println("request fail:", err.Error())
+			return err
+		}
+		b2 := resp.Body()
+		if len(b1) == len(b2) {
+			fmt.Println("Heuristics reveal endpoint might be VULNERABLE to Referer CSRFs...")
+		}
+	}
+	return nil
+}
