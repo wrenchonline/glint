784be267faf7d775a5092248b23612da7cf7abda
wrenchoniline <ljl260435988@gmail.com>
2021-11-22 17:24:37 +0800

    add domain collect

--- crawler/crawtask.go
+++ crawler/crawtask.go
@@ -3,6 +3,7 @@ package crawler
 import (
 	"sync"
 	"time"
+	"wenscan/log"
 	"wenscan/model"
 
 	"github.com/panjf2000/ants/v2"
@@ -78,5 +79,116 @@ func (t *CrawlerTask) generateTabTask(req *model.Request) *tabTask {
 func (t *CrawlerTask) Run() {
 	defer t.Pool.Release()  // 释放协程池
 	defer t.Browser.Close() // 关闭浏览器
+	if t.Config.PathFromRobots {
+		reqsFromRobots := GetPathsFromRobots(*t.Targets[0])
+		log.Info("get paths from robots.txt: ", len(reqsFromRobots))
+		t.Targets = append(t.Targets, reqsFromRobots...)
+	}
+	if t.Config.FuzzDictPath != "" {
+		if t.Config.PathByFuzz {
+			log.Warning("`--fuzz-path` is ignored, using `--fuzz-path-dict` instead")
+		}
+		reqsByFuzz := GetPathsByFuzzDict(*t.Targets[0], t.Config.FuzzDictPath)
+		t.Targets = append(t.Targets, reqsByFuzz...)
+	} else if t.Config.PathByFuzz {
+		reqsByFuzz := GetPathsByFuzz(*t.Targets[0])
+		log.Info("get paths by fuzzing: ", len(reqsByFuzz))
+		t.Targets = append(t.Targets, reqsByFuzz...)
+	}
+	t.Result.AllReqList = t.Targets[:]
+	var initTasks []*model.Request
 
+	for _, req := range t.Targets {
+		if t.smartFilter.DoFilter(req) {
+			log.Debug("filter req: %s", req.URL.RequestURI())
+			continue
+		}
+		initTasks = append(initTasks, req)
+		t.Result.ReqList = append(t.Result.ReqList, req)
+	}
+	log.Info("filter repeat, target count: ", len(initTasks))
+
+	for _, req := range initTasks {
+		if !FilterKey(req.URL.String(), t.Config.IgnoreKeywords) {
+			t.addTask2Pool(req)
+		}
+	}
+
+	t.taskWG.Wait()
+
+	// 全部域名
+	t.Result.AllDomainList = AllDomainCollect(t.Result.AllReqList)
+	// 子域名
+	t.Result.SubDomainList = SubDomainCollect(t.Result.AllReqList, t.RootDomain)
+}
+
+/**
+添加任务到协程池
+添加之前实时过滤
+*/
+func (t *CrawlerTask) addTask2Pool(req *model.Request) {
+	t.taskCountLock.Lock()
+	if t.crawledCount >= t.Config.MaxCrawlCount {
+		t.taskCountLock.Unlock()
+		return
+	} else {
+		t.crawledCount += 1
+	}
+	t.taskCountLock.Unlock()
+
+	t.taskWG.Add(1)
+	task := t.generateTabTask(req)
+	go func() {
+		err := t.Pool.Submit(task.Task)
+		if err != nil {
+			t.taskWG.Done()
+			log.Error("addTask2Pool ", err)
+		}
+	}()
+}
+
+/**
+单个运行的tab标签任务，实现了workpool的接口
+*/
+func (t *tabTask) Task() {
+	defer t.crawlerTask.taskWG.Done()
+	tab := engine2.NewTab(t.browser, *t.req, engine2.TabConfig{
+		TabRunTimeout:           t.crawlerTask.Config.TabRunTimeout,
+		DomContentLoadedTimeout: t.crawlerTask.Config.DomContentLoadedTimeout,
+		EventTriggerMode:        t.crawlerTask.Config.EventTriggerMode,
+		EventTriggerInterval:    t.crawlerTask.Config.EventTriggerInterval,
+		BeforeExitDelay:         t.crawlerTask.Config.BeforeExitDelay,
+		EncodeURLWithCharset:    t.crawlerTask.Config.EncodeURLWithCharset,
+		IgnoreKeywords:          t.crawlerTask.Config.IgnoreKeywords,
+		CustomFormValues:        t.crawlerTask.Config.CustomFormValues,
+		CustomFormKeywordValues: t.crawlerTask.Config.CustomFormKeywordValues,
+	})
+	tab.Start()
+
+	// 收集结果
+	t.crawlerTask.Result.resultLock.Lock()
+	t.crawlerTask.Result.AllReqList = append(t.crawlerTask.Result.AllReqList, tab.ResultList...)
+	t.crawlerTask.Result.resultLock.Unlock()
+
+	for _, req := range tab.ResultList {
+		if t.crawlerTask.Config.FilterMode == config.SimpleFilterMode {
+			if !t.crawlerTask.smartFilter.SimpleFilter.DoFilter(req) {
+				t.crawlerTask.Result.resultLock.Lock()
+				t.crawlerTask.Result.ReqList = append(t.crawlerTask.Result.ReqList, req)
+				t.crawlerTask.Result.resultLock.Unlock()
+				if !FilterKey(req.URL.String(), t.crawlerTask.Config.IgnoreKeywords) {
+					t.crawlerTask.addTask2Pool(req)
+				}
+			}
+		} else {
+			if !t.crawlerTask.smartFilter.DoFilter(req) {
+				t.crawlerTask.Result.resultLock.Lock()
+				t.crawlerTask.Result.ReqList = append(t.crawlerTask.Result.ReqList, req)
+				t.crawlerTask.Result.resultLock.Unlock()
+				if !FilterKey(req.URL.String(), t.crawlerTask.Config.IgnoreKeywords) {
+					t.crawlerTask.addTask2Pool(req)
+				}
+			}
+		}
+	}
 }
