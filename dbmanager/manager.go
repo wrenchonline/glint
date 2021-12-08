4d2f18b45e292b80f67f162afe1cf276c2fa38df
John Doe <ljl260435988@gmail.com>
2021-12-09 00:31:07 +0800

    fix the db struct for db manager

--- dbmanager/manager.go
+++ dbmanager/manager.go
@@ -1,6 +1,7 @@
 package dbmanager
 
 import (
+	"database/sql"
 	"glint/config"
 	"glint/log"
 	"strings"
@@ -11,6 +12,31 @@ import (
 
 type DbManager struct {
 	Db *sqlx.DB
+}
+
+type DbTaskConfig struct {
+	TaskId                      sql.NullInt32  `db:"TaskId"`
+	FilterMode                  sql.NullString `db:"FilterMode"`
+	ExtraHeadersUuid            sql.NullString `db:"ExtraHeadersUuid"`
+	AllDomainReturn             sql.NullBool   `db:"AllDomainReturn"`
+	SubDomainReturn             sql.NullBool   `db:"SubDomainReturn"`
+	IncognitoContext            sql.NullBool   `db:"IncognitoContext"`
+	NoHeadless                  sql.NullBool   `db:"NoHeadless"`
+	DomContentLoadedTimeout     sql.NullInt32  `db:"DomContentLoadedTimeout"`
+	TabRunTimeout               sql.NullInt32  `db:"TabRunTimeout"`
+	PathByFuzz                  sql.NullBool   `db:"PathByFuzz"`
+	FuzzDictPath                sql.NullBool   `db:"FuzzDictPath"`
+	PathFromRobots              sql.NullBool   `db:"PathFromRobots"`
+	MaxTabsCount                sql.NullInt32  `db:"MaxTabsCount"`
+	ChromiumPath                sql.NullString `db:"ChromiumPath"`
+	EventTriggerMode            sql.NullString `db:"EventTriggerMode"`
+	EventTriggerInterval        sql.NullInt32  `db:"EventTriggerInterval"`
+	EncodeURLWithCharset        sql.NullBool   `db:"EncodeURLWithCharset"`
+	IgnoreKeywords              sql.NullString `db:"IgnoreKeywords"`
+	Proxy                       sql.NullString `db:"Proxy"`
+	CustomFormValuesUuid        sql.NullString `db:"CustomFormValuesUuid"`
+	CustomFormKeywordValuesUuid sql.NullString `db:"CustomFormKeywordValuesUuid"`
+	XssPayloadsUuid             sql.NullString `db:"XssPayloadsUuid"`
 }
 
 //Init 初始化mysql数据库
@@ -31,7 +57,7 @@ func (Dm *DbManager) Init() error {
 }
 
 //GetTaskConfig 根据任务ID获取数据库的扫描配置
-func (Dm *DbManager) GetTaskConfig(taskid int) ([]interface{}, error) {
+func (Dm *DbManager) GetTaskConfig(taskid int) ([]DbTaskConfig, error) {
 	sql := `
 	SELECT
 	task_config.*
@@ -40,7 +66,7 @@ func (Dm *DbManager) GetTaskConfig(taskid int) ([]interface{}, error) {
 	WHERE
 		task_config.TaskId = ?
 	`
-	var values []interface{}
+	values := []DbTaskConfig{}
 	err := Dm.Db.Select(&values, sql, taskid)
 	if err != nil {
 		log.Error(err.Error())
