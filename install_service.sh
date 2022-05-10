306464515974d063519917e3ffe90b24e82da489
wrenchoniline <ljl260435988@gmail.com>
2022-05-10 14:25:46 +0800

    5/10 part 3

--- install_service.sh
+++ install_service.sh
@@ -1,5 +1,9 @@
 chmod +x ./glint.service
 cp ./glint.service /etc/systemd/system/glint.service
+mkdir /usr/local/863
+mkdir /usr/local/863/certific
+cp ./server.key /usr/local/863/certific
+cp ./server.pem /usr/local/863/certific
 systemctl daemon-reload 
 systemctl enable glint
 systemctl start glint
